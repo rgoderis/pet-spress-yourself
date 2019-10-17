19906471c5353b153340a417a6707fe391182235
Ryan Goderis <rgoderis789@gmail.com>
2019-10-17 11:35:27 -0400

    added userName property to User model

--- models/user.js
+++ models/user.js
@@ -2,6 +2,11 @@ var bcrypt = require("bcryptjs");
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
+    userName: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
     email: {
       type: DataTypes.STRING,
       allowNull: false,

bccfc49ef3a2f21e65e4a85343a0c2200708f46c
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-10-17 11:46:15 -0400

    changed values of questionnaire answers in html. updated sign up and login html and client side JS to include user name.

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
   var loginForm = $("form.login");
+  var userNameInput = $("input#userName");
   var emailInput = $("input#userEmail");
   var passwordInput = $("input#userPassword");
   loginForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
+      userName: userNameInput.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
-    if (!userData.email || !userData.password) {
+    if (!userData.userName || !userData.email || !userData.password) {
       return;
     }
-    loginUser(userData.email, userData.password);
+    loginUser(userData.userName, userData.email, userData.password);
     emailInput.val("");
     passwordInput.val("");
   });
-  function loginUser(email, password) {
+  function loginUser(userName, email, password) {
     $.post("/api/login", {
+      userName: userName,
       email: email,
       password: password
     })

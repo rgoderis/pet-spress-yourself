2cbf8759f54608dd3c48d421d08c911c6cc69689
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-10-18 11:43:53 -0400

    added jQuery to update favorite button once clicked.

--- public/js/results.js
+++ public/js/results.js
@@ -1,6 +1,9 @@
 $(".fav-btn").on("click", function(event) {
   var animalId = $(this).attr("data-id");
 
+  $(this).attr("disabled", true);
+  $(this).html("Added to Favorites!");
+
   var favAnimal = {
     animalId: animalId
   };
@@ -8,7 +11,7 @@ $(".fav-btn").on("click", function(event) {
   $.ajax("/api/favorites/" + $("h3").attr("data-name"), {
     type: "POST",
     data: favAnimal
-  }).then(function(data) {
+  }).then(function() {
     location.reload();
   });
 });
